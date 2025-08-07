3aedfda433d64bd5725fa85a761bdb77ce2249be
aliNzLami <ali.nabizadeh79@yahoo.com>
2025-08-07 23:29:51 +0330

    bundled project

---
+++ ac6f5da3eba0622b5961.js
@@ -0,0 +1,1 @@
+import"./bootstrap.bundle.js";import"./language.js";import"./DOMlist.js";import"./content.js";import"./layout.js";import"./book.js";import{rednerLanguage}from"./language.js";import{renderLayout}from"./layout.js";rednerLanguage.init(),renderLayout.init();
